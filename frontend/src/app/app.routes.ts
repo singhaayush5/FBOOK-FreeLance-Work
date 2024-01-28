b27a77d24ad312cd734af745c058eb23443dbff3
Anish <anishkumarsingh45@gmail.com>
2024-01-28 11:42:30 +0530

    login added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   //   { path: '', redirectTo: '', pathMatch: 'full' },
 ];
